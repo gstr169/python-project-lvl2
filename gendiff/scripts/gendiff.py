ee316fe1595e3b8c32f044533b096535283e681b
d.finko <d.finko@x5.ru>
2020-10-25 00:17:29 +0300

    completed step 2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
+
+args = parser.parse_args()
